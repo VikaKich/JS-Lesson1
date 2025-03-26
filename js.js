f43abfaf7db3fd341e59144dab4a6cbd19ef0782
VikaKich <vika.230498@gmail.com>
2025-03-26 13:00:29 +0200

    Added Ex3 - constants

--- js.js
+++ js.js
@@ -6,8 +6,14 @@
 // console.log(myArr);
 
 //alert, prompt, confirm exercises----------------------
-let aName = prompt('What is your name?');
-alert(`Your name is ${aName}!`);
-if (confirm('Do you want to delete the product from the list?')) {
-  console.log('The product was deleted.');
-}
+// let aName = prompt('What is your name?');
+// alert(`Your name is ${aName}!`);
+// if (confirm('Do you want to delete the product from the list?')) {
+//   console.log('The product was deleted.');
+// }
+
+//Constants exercise
+let admin, name;
+name = 'John';
+admin = name;
+console.log(admin);
