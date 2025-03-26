73e0644937f2cb199b8d6421903eb1e7472226f7
VikaKich <vika.230498@gmail.com>
2025-03-26 12:59:17 +0200

    Added the solution for (alert, prompt, confirm) ex

--- js.js
+++ js.js
@@ -1,5 +1,13 @@
-document.write('Viktoria Kicherman');
-const myName = { firstname: 'Viktoria', lastname: 'Kicherman' };
-console.log(myName);
-const myArr = ['Meital', 'Liran', 'Viktoria'];
-console.log(myArr);
+//Output information exercises------------------------
+// document.write('Viktoria Kicherman');
+// const myName = { firstname: 'Viktoria', lastname: 'Kicherman' };
+// console.log(myName);
+// const myArr = ['Meital', 'Liran', 'Viktoria'];
+// console.log(myArr);
+
+//alert, prompt, confirm exercises----------------------
+let aName = prompt('What is your name?');
+alert(`Your name is ${aName}!`);
+if (confirm('Do you want to delete the product from the list?')) {
+  console.log('The product was deleted.');
+}
