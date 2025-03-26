18ac07a564027a2426584b770713a797bae18847
VikaKich <vika.230498@gmail.com>
2025-03-26 11:46:11 +0200

    Added index.html and js.js

---
+++ js.js
@@ -0,0 +1,5 @@
+document.write('Viktoria Kicherman');
+const myName = { firstname: 'Viktoria', lastname: 'Kicherman' };
+console.log(myName);
+const myArr = ['Meital', 'Liran', 'Viktoria'];
+console.log(myArr);
